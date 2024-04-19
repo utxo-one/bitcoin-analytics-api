13c239422d56d23ecd2108102d09426be3046c2b
utxo <github@utxo.one>
2024-04-18 20:04:26 -0400

    Feature: Electrum API

--- apps/bitcoin-analytics-api/src/address/address.controller.ts
+++ apps/bitcoin-analytics-api/src/address/address.controller.ts
@@ -2,9 +2,9 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { UseInterceptors } from '@nestjs/common/decorators';
 import { AddressCacheInterceptor } from './address.cache.interceptor';
+import { ElectrumService } from '../electrum/electrum.service';
 
 @Controller('address')
-@UseInterceptors(AddressCacheInterceptor)
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
--- apps/bitcoin-analytics-api/src/address/address.service.ts
+++ apps/bitcoin-analytics-api/src/address/address.service.ts
@@ -1,49 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { ElectrumService } from '../electrum/electrum.service';
 
 @Injectable()
 export class AddressService {
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly electrumService: ElectrumService,
+  ) {}
 
   async getAddressSummary(addressObj): Promise<any> {
     const address = addressObj.address;
 
-    const query = `
-    SELECT
-        JSON_OBJECT(
-            'address', ?,
-            'transactions', (
-                SELECT JSON_ARRAYAGG(
-                    JSON_OBJECT(
-                        'id', t.id,
-                        'txid', t.txid,
-                        'hash', t.hash,
-                        'size', t.size,
-                        'vsize', t.vsize,
-                        'weight', t.weight,
-                        'locktime', t.locktime,
-                        'blockId', t.blockId,
-                        'time', b.time,
-                        'blocktime', b.time
-                    )
-                )
-                FROM transactions t
-                JOIN blocks b ON t.blockId = b.id
-                WHERE t.id IN (
-                    SELECT transactionId FROM transaction_outputs WHERE scriptPubKeyAddress = ?
-                    UNION
-                    SELECT i.transactionId FROM transaction_inputs i
-                    JOIN transaction_outputs o ON i.txid = o.transactionId AND i.vout = o.n
-                    WHERE o.scriptPubKeyAddress = ?
-                )
-            )
-        ) AS summary
-    `;
+    const addressTransactions =
+      await this.electrumService.getBlockHeight(address);
 
-    const parameters = [address, address, address];
-
-    const rawResults = await this.dataSource.query(query, parameters);
-
-    return rawResults.map((row) => row.summary);
+    return addressTransactions;
   }
 }
